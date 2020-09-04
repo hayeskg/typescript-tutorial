a5f814b649975593e029b2f2425930c27f37fa84
Kristof Hayes <kristof@e-nano.io>
2020-09-04 13:14:37 +0200

    interfaces extended

--- filterByTerm.js
+++ filterByTerm.js
@@ -1,12 +1,24 @@
 "use strict";
-function filterByTerm(input, searchTerm) {
+function filterByTerm(input, searchTerm, lookupKey) {
+    if (lookupKey === void 0) { lookupKey = "url"; }
     if (!searchTerm)
         throw Error("searchTerm cannot be empty");
     if (!input.length)
         throw Error("inputArr cannot be empty");
     var regex = new RegExp(searchTerm, "i");
     return input.filter(function (arrayElement) {
-        return arrayElement.url.match(regex);
+        return arrayElement[lookupKey].match(regex);
     });
 }
-filterByTerm([{ url: "string1" }, { url: "string2" }, { url: "string3" }], "java");
+var obj1 = { url: "string1" };
+var obj2 = { url: "string2" };
+var obj3 = { url: "string3" };
+var arrOfLinks = [obj1, obj2, obj3];
+var term = "java";
+var link1 = {
+    description: "developing cool stuff",
+    id: 1,
+    url: "www.kristofhayes.me",
+    language: "en"
+};
+filterByTerm(arrOfLinks, "string3");
--- filterByTerm.ts
+++ filterByTerm.ts
@@ -1,17 +1,43 @@
 interface Link {
+  description?: string;
+  id?: number;
   url: string;
+  [index: string]: any;
 }
 
-function filterByTerm(input: Array<Link>, searchTerm: string) {
+interface TranslatedLink extends Link {
+  language: string;
+}
+
+type Links = Array<Link>;
+
+function filterByTerm(
+  input: Array<Link>,
+  searchTerm: string,
+  lookupKey: string = "url"
+): Array<Link> {
   if (!searchTerm) throw Error("searchTerm cannot be empty");
   if (!input.length) throw Error("inputArr cannot be empty");
   const regex = new RegExp(searchTerm, "i");
   return input.filter(function (arrayElement) {
-    return arrayElement.url.match(regex);
+    return arrayElement[lookupKey].match(regex);
   });
 }
 
-filterByTerm(
-  [{ url: "string1" }, { url: "string2" }, { url: "string3" }],
-  "java"
-);
+const obj1: Link = { url: "string1" };
+const obj2: Link = { url: "string2" };
+const obj3: Link = { url: "string3" };
+
+const arrOfLinks: Array<Link> = [obj1, obj2, obj3];
+
+const term: string = "java";
+
+const link1: TranslatedLink = {
+  description:
+    "developing cool stuff",
+  id: 1,
+  url: "www.kristofhayes.me",
+  language: "en"
+};
+
+filterByTerm(arrOfLinks, "string3");
